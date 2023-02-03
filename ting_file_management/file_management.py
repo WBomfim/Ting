8349ea4e8fd324899b0590e8f69b89d9f7bf7708
WBomfim <willianbomfim@hotmail.com>
2023-02-03 20:28:37 -0300

    feat: Implementado a função txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if path_file.endswith(".txt"):
+            with open(path_file, "r") as file:
+                return list(file.read().splitlines())
+
+        return sys.stderr.write("Formato inválido")
+
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
