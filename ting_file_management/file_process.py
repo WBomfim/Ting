e6ce916e971edd0a580834990f6d645bb7cebf83
WBomfim <willianbomfim@hotmail.com>
2023-02-03 21:16:45 -0300

    feat: Implementado a função process - A função é responsável por manipular os dados retornado da função txt_import e inserir um dicionário na fila de processos.

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    if any(file['nome_do_arquivo'] == path_file for file in instance._queue):
+        return
+
+    data = txt_importer(path_file)
+    dict_file = {
+        'nome_do_arquivo': path_file,
+        'qtd_linhas': len(data),
+        'linhas_do_arquivo': data
+    }
+
+    instance.enqueue(dict_file)
+    return sys.stdout.write(f"{dict_file}\n")
 
 
 def remove(instance):
