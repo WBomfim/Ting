87509794b46a5a464143d9191d5ccee7a3b50f68
WBomfim <willianbomfim@hotmail.com>
2023-02-03 23:38:14 -0300

    refactor: refatorado a função exists_word - Para a alteração foram criado duas funções auxiliares, a generate_report_word_in_file e report_word_in_file, dessa forma podemos alterar o tipo de report utilizando a mesma função para gerar o relatório.

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,12 +1,18 @@
-def exists_word(word, instance):
+def report_word_in_file(word, file):
+    occurrence_lines = []
+
+    for index, line in enumerate(file['linhas_do_arquivo']):
+        if word.lower() in line.lower():
+            occurrence_lines.append({'linha': index + 1})
+
+    return occurrence_lines
+
+
+def generate_report_word_in_file(word, instance, report_method):
     results = []
 
     for file in instance._queue:
-        occurrence_lines = []
-
-        for index, line in enumerate(file['linhas_do_arquivo']):
-            if word.lower() in line.lower():
-                occurrence_lines.append({'linha': index + 1})
+        occurrence_lines = report_method(word, file)
 
         if occurrence_lines:
             results.append({
@@ -16,6 +22,10 @@ def exists_word(word, instance):
                     })
 
     return results
+
+
+def exists_word(word, instance):
+    return generate_report_word_in_file(word, instance, report_word_in_file)
 
 
 def search_by_word(word, instance):
