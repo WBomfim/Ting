efd5872378864b2195859523ef6c799dcfef84ce
WBomfim <willianbomfim@hotmail.com>
2023-02-03 21:47:09 -0300

    feat: Implementado a função file_metadata - A função é responsável por exibir as informações contidas em uma determinada posição da fila.

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -28,4 +28,8 @@ def remove(instance):
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        return sys.stdout.write(f"{instance.search(position)}\n")
+
+    except IndexError:
+        return sys.stderr.write("Posição inválida\n")
