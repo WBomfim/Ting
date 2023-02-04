e971de1848137d73e94ebc10faae7dff681e64bf
WBomfim <willianbomfim@hotmail.com>
2023-02-03 23:17:06 -0300

    feat: Implementado a função exists_word - A função é responsável por pesquisar uma palavra dentro dos arquivos da fila e retornar uma lista com informações sobre a pesquisa.

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,21 @@
 def exists_word(word, instance):
-    """Aquiii irá sua implementação"""
+    results = []
+
+    for file in instance._queue:
+        occurrence_lines = []
+
+        for index, line in enumerate(file['linhas_do_arquivo']):
+            if word.lower() in line.lower():
+                occurrence_lines.append({'linha': index + 1})
+
+        if occurrence_lines:
+            results.append({
+                        'palavra': word,
+                        'arquivo': file['nome_do_arquivo'],
+                        'ocorrencias': occurrence_lines
+                    })
+
+    return results
 
 
 def search_by_word(word, instance):
